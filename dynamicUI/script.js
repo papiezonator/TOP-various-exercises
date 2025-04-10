bd6603df7feaef898eea8c09a5c17bf21dbf9d5d
papiezonator <52249311+papiezonator@users.noreply.github.com>
2025-04-10 12:01:33 +0200

    dropdown items are visible after clicking on the main div

--- dynamicUI/script.js
+++ dynamicUI/script.js
@@ -1,1 +1,20 @@
-console.log("hi");
+
+
+const parent = document.querySelector(".wrapper");
+let main = parent.childNodes[1];
+
+main.addEventListener("click", () => {
+    //console.log("here", parent.childNodes[0]);
+    for(let i = 3; i < parent.childNodes.length; i++){
+        if(i % 2){
+        console.log("e", parent.childNodes[i]);
+        parent.childNodes[i].style.visibility = "visible";
+        }
+    
+    };
+    return;
+    
+})
+
+//console.log(parent.childNodes[1]);
+
